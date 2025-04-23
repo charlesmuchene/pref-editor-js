604e59ea871ee043a541f9ab6c201a5eea2814ef
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2025-04-22 21:23:30 -0500

    Add basic adb command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Let us bridge the world!");
+import Adb from "@devicefarmer/adbkit";
+import type { Device } from "@devicefarmer/adbkit";
+
+const client = Adb.createClient();
+
+client
+  .listDevices()
+  .then((devices: Device[]) =>
+    Promise.all(
+      devices.map(async (device) => {
+        const adbDevice = client.getDevice(device.id);
+        const stream = await adbDevice.shell("seq -w 16 | xargs -n 4 echo");
+        const output = await Adb.util.readAll(stream);
+        console.log("[%s]\n%s", device.id, output.toString().trim());
+      })
+    )
+  )
+  .then(() => console.log("\nDone bridging."))
+  .catch((err: Error) => console.error("See what went wrong:", err.stack));
